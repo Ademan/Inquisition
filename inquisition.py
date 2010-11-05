1545caf2cb0d0d8d4589f22f15b045651ffa46d0
Daniel Roberts <Ademan555@gmail.com>
2010-11-05 02:12:02 -0700

    I think inquisition.py is going to have to die.

--- inquisition.py
+++ inquisition.py
@@ -12,40 +12,6 @@ logger = LoggingCallLogger(logging.getLogger())
 
 cclogger = DecorateAll(logger.log_call)
 
-import urwid
-
-class ObjectInspector(object):
-    palette = [('body', 'default', 'black'),
-               ('name', 'dark green', 'black'),
-               ('object', 'dark blue', 'black'),
-               ('primitive', 'dark red', 'black'),
-               ('method', 'yellow', 'black'),
-               ]
-
-    def __init__(self, root_node):
-        self.treelistbox = urwid.TreeListBox(urwid.TreeWalker(root_node))
-        self.treelistbox.offset_rows = 1
-
-    def unhandled_keys(self, key):
-        try:
-            if key in 'qQ':
-                raise urwid.ExitMainLoop
-        except TypeError, e: pass
-
-    def loop(self):
-        self.loop = urwid.MainLoop(urwid.AttrWrap(self.treelistbox, 'body'),
-                                   self.palette, unhandled_input=self.unhandled_keys)
-        return self.loop
-
-def inspect_object(obj, name='Root Object'):
-    from nodes import ObjectNode
-    node = ObjectNode(name, obj)
-    ObjectInspector(node).loop().run()
-
-def inspect_dictionary(dict, name="Root Dictionary"):
-    from nodes import DictionaryNode
-    node = DictionaryNode(name, dict)
-    ObjectInspector(node).loop().run()
-
 if __name__ == '__main__':
+    from inquisition import inspect_dictionary
     inspect_dictionary(locals(), 'locals')
