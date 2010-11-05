5137807bf480137fba0c392c8daf65a83cfda8cb
Daniel Roberts <Ademan555@gmail.com>
2010-11-05 01:23:09 -0700

    Hopefully a better setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='inquisition',
       version='0.1',
       description='Python Object Inspector',
       author='Daniel Roberts',
       author_email='Ademan555@gmail.com',
+      scripts=['inquisition.py'],
       packages=['inquisition'],
      )
